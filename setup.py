f3101f9851d27076cab4330c0cbccf04c536cc14
Bryan Cinman <syngenius@gmail.com>
2015-02-24 17:23:14 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='strato',
+    description='An experiment in web frameworks',
+    version='0.0.1',
+    url='https://github.com/bcinman/strato',
+    author='Bryan Cinman',
+    author_email='syngenius@gmail.com',
+    license='MIT'
+)
