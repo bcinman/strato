f0d55ae6ac06805047b306989b57f1e081e580d6
Bryan Cinman <syngenius@gmail.com>
2015-02-25 14:20:09 -0500

    response status code is supposed to be a string for some reason

--- strato/response.py
+++ strato/response.py
@@ -1,8 +1,15 @@
 def text(body, status=200):
-    return status, body, [('Content-type', 'text/plain')]
+    return response(body, status, 'text/plain')
 
 def html(body, status=200):
-    return status, body, [('Content-type', 'text/html')]
+    return response(body, status, 'text/html')
 
 def json(body, status=200):
-    return status, body, [('Content-type', 'application/json')]
+    return response(body, status, 'application/json')
+
+def response(body, status=200, content_type='text/plain', headers=None):
+    if headers is None:
+        headers = []
+    headers.append(('Content-type', content_type))
+    status = str(status)
+    return status, body, headers
